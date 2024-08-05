292fbf28fa895177bb7f1a1716c8010c57fe2627
salim BENABDELLAH <salim.benabdellah@hotmail.fr>
2024-08-05 16:23:33 +0200

    Add project files.

---
+++ TPCalculatrice/Calculatrice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPCalculatrice
+{
+    internal class Calculatrice
+    {
+        
+        private int Resultas { get; set; }
+        
+        
+
+        public int Addition(int a, int b) 
+        { return Resultas = a + b; }
+
+        public int Soustraction(int a , int b)
+        {
+            return Resultas = a - b;
+        }
+        public int  Division( int a, int b)
+        {
+            if (b != 0)
+            {
+                
+                    return Resultas = a / b;
+            }
+
+            return Resultas = 0;
+            
+
+        } 
+        public int Multiplication( int a , int b)
+        {
+            return Resultas = a* b;
+        }
+
+        public int Modulo(int a , int b)
+        {
+            return Resultas = a % b;
+
+        }
+    }
+}
---
+++ TPCalculatrice/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using TPCalculatrice;
+
+
+int v;
+int w;
+while (true)
+{
+    Console.WriteLine("introduit le premier numéro");
+    string a = Console.ReadLine();
+
+    try 
+    {
+         v = int.Parse(a);
+        
+       break;
+    }
+    catch
+    {
+        Console.WriteLine(" Svp veuillez réessayer  car la valeur saisie n’est pas acceptable ");
+    }
+}
+while (true)
+{
+    Console.WriteLine("introduit le deuxieme numéro ");
+    string b = Console.ReadLine();
+
+    try
+    {
+        w = int.Parse(b);
+
+        break;
+    }
+    catch
+    {
+        Console.WriteLine(" Svp veuillez réessayer  car la valeur saisie n’est pas acceptable ");
+    }
+}
+
+  
+
+
+
+Calculatrice calculatrice = new Calculatrice();
+
+
+
+Console.WriteLine(calculatrice.Addition(v,w));
+Console.WriteLine(calculatrice.Soustraction(v,w));
+Console.WriteLine(calculatrice.Division(v,w));
+Console.WriteLine(calculatrice.Multiplication(v,w));
+Console.WriteLine(calculatrice.Modulo(v,w));
